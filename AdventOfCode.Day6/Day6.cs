caef983d0923237c3d6fc4d2517817026fa9c9c2
nils-larsen <nilsfredriklarsen@gmail.com>
2017-12-06 22:10:32 +0100

    Day6 finished (ugly as hell)

---
+++ AdventOfCode.Day6/Day6.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Day6
+{
+    public class Day6
+    {
+        public string GetOneRedistribution(string input)
+        {
+            var distribution = input.Split().Select(int.Parse).ToList();
+
+            var index = distribution.IndexOf(distribution.Max());
+            var blocks = distribution[index];
+
+            distribution[index] = 0;
+            index++;
+
+            while (blocks > 0)
+            {
+                if (index > distribution.Count - 1)
+                    index = 0;
+
+                distribution[index]++;
+
+                index++;
+                blocks--;
+            }
+            return string.Join(" ", distribution);
+        }
+
+        public int NumberOfValidRedistributions(string input)
+        {
+            return AllRedistributions(input).Count;
+        }
+
+        private List<string> AllRedistributions(string input)
+        {
+            var redistributions = new List<string>();
+            while (!redistributions.GroupBy(x => x).Any(x => x.Skip(1).Any()))
+            {
+                var result = GetOneRedistribution(input);
+                redistributions.Add(result);
+                input = result;
+            }
+
+            return redistributions;
+        }
+
+        public int NumberOfRedistributionsBetweenFirstDuplicate(string input)
+        {
+            var redistributions = AllRedistributions(input);
+            var indexOfLastDuplicate = redistributions.Last();
+
+            var indexOfFirstDuplicate = redistributions
+                .FindIndex(x => x == indexOfLastDuplicate);
+
+            return redistributions.Count - 1 - indexOfFirstDuplicate;
+        }
+    }
+
+}
---
+++ AdventOfCode.Day6/Day6Tests.cs
@@ -0,0 +1,73 @@
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Xunit;
+
+namespace AdventOfCode.Day6
+{
+    public class TestDay6
+    {
+        readonly Day6 _day6;
+
+        public TestDay6()
+        {
+            _day6 = new Day6();
+        }
+
+        [Fact]
+        public void Part1_GetResult()
+        {
+            var input = string
+                .Join(" ", Regex
+                    .Split(File.ReadAllText("Day6.txt"), @"\s+")
+                    .Where(s => s != string.Empty));
+
+            var actual = _day6.NumberOfValidRedistributions(input);
+
+            Assert.Equal(5042, actual);
+        }
+
+        [Fact]
+        public void Part2_GetResult()
+        {
+            var input = string
+                .Join(" ", Regex
+                      .Split(File.ReadAllText("Day6.txt"), @"\s+")
+                      .Where(s => s != string.Empty));
+
+            var actual = _day6.NumberOfRedistributionsBetweenFirstDuplicate(input);
+
+            Assert.Equal(1086, actual);
+        }
+
+        [Theory]
+        [InlineData("0 2 7 0", "2 4 1 2")]
+        [InlineData("2 4 1 2", "3 1 2 3")]
+        [InlineData("3 1 2 3", "0 2 3 4")]
+        [InlineData("0 2 3 4", "1 3 4 1")]
+        [InlineData("1 3 4 1", "2 4 1 2")]
+        public void Part1_TestOneAtATime(string input, string expected)
+        {
+            var actual = _day6.GetOneRedistribution(input);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("0 2 7 0", 5)]
+        public void Part1_TestWhole(string input, int expected)
+        {
+            var actual = _day6.NumberOfValidRedistributions(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("0 2 7 0", 4)]
+        public void Part2_NumberOfRedistributionsBetweenFirstDuplicate(string input, int expected)
+        {
+            var actual = _day6.NumberOfRedistributionsBetweenFirstDuplicate(input);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
