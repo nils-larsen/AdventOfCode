1d8098467163c847921cde908fd971377e0428bd
nils-larsen <nilsfredriklarsen@gmail.com>
2017-12-07 22:52:25 +0100

    Day 7 part 1 finished

---
+++ AdventOfCode.Day7/Day7Tests.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using Xunit;
+
+namespace AdventOfCode.Day7
+{
+    public class TestDay7
+    {
+        readonly Day7 _day7;
+
+        public TestDay7()
+        {
+            _day7 = new Day7();
+        }
+
+        [Fact]
+        public void Part1_GetResult()
+        {
+            var input = File.ReadLines("Day7.txt");
+
+            var actual = _day7.FindBottomProgram(input);
+
+            Assert.Equal("qibuqqg", actual);
+        }
+
+        [Fact]
+        public void Part1_TestSampleData()
+        {
+            var input = File.ReadLines("Day7Test.txt");
+
+            var actual = _day7.FindBottomProgram(input);
+
+            Assert.Equal("tknk", actual);
+        }
+    }
+}
