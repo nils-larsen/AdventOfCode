3251267b47b9a1e1e673f82e2dae8b794aff7539
nils-larsen <nilsfredriklarsen@gmail.com>
2017-12-08 18:39:04 +0100

    Had to create a new day7 project since something weird happened. Still only part1 finished

---
+++ AdventOfCode.Day7/Day7.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Day7
+{
+    public class Day7
+    {
+        public string FindBottomProgram(IEnumerable<string> input)
+        {
+            var linesWithParents = input.Select(x => x).Where(x => x.Contains("->"));
+
+            var parents = linesWithParents.Select(x => x.Split().First());
+
+            var children = string
+                .Join(" ", linesWithParents
+                      .Select(x => string.Join(" ", x.Replace(",", "").Split().Skip(3))))
+                .Split();
+
+            return parents.Except(children).First();
+        }
+    }
+}
--- AdventOfCode.Day7/Day7Tests.cs
+++ AdventOfCode.Day7/Day7Tests.cs
@@ -12,32 +12,24 @@ namespace AdventOfCode.Day7
             _day7 = new Day7();
         }
 
-        //[Fact]
-        //public void Part1_GetResult()
-        //{
-        //    var input = File.ReadLines("Day7.txt");
-
-        //    var actual = _day7.FindBottomProgram(input);
-
-        //    Assert.Equal("qibuqqg", actual);
-        //}
-
-        //[Fact]
-        //public void Part1_TestSampleData()
-        //{
-        //    var input = File.ReadLines("Day7Test.txt");
-
-        //    var actual = _day7.FindBottomProgram(input);
-
-        //    Assert.Equal("tknk", actual);
-        //}
-
         [Fact]
-        public void Part1_TestWithTree()
+        public void Part1_GetResult()
         {
-            var tree = new Tree();
-            tree.ParseText("Day7Test.txt");
+            var input = File.ReadLines("Day7.txt");
+
+            var actual = _day7.FindBottomProgram(input);
+
+            Assert.Equal("qibuqqg", actual);
         }
 
+        [Fact]
+        public void Part1_TestSampleData()
+        {
+            var input = File.ReadLines("Day7Test.txt");
+
+            var actual = _day7.FindBottomProgram(input);
+
+            Assert.Equal("tknk", actual);
+        }
     }
 }
