97841ab7ee48f08e062ca1224cb6b482032d6cc6
nils-larsen <nilsfredriklarsen@gmail.com>
2017-12-17 16:25:39 +0100

    Day 17 finished

---
+++ AdventOfCode.Day17/Spinlock.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode.Day17
+{
+    public static class Spinlock
+    {
+        public static int StartInsertion(int stepSize)
+        {
+            var position = 0;
+            var list = new List<int>(2018);
+            list.Insert(position, 0);
+
+            for (var length = 1; length < 2018; length++)
+            {
+                position = (stepSize + position) % length;
+                list.Insert(position, length);
+                position++;
+
+                if (length == 2017)
+                {
+                    return list[position];
+                }
+            }
+            return 0;
+        }
+
+        public static int Part2(int stepSize)
+        {
+            int position = 0;
+            int result = 0;
+            for (int length = 1; length < 50000001; length++)
+            {
+                position = (position + stepSize) % length;
+                position++;
+
+                if (position == 1)
+                {
+                    result = length;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ AdventOfCode.Day17/UnitTest1.cs
@@ -0,0 +1,35 @@
+using Xunit;
+using FluentAssertions;
+
+namespace AdventOfCode.Day17
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Part1_GetResult()
+        {
+            Spinlock
+                .StartInsertion(349)
+                .Should()
+                .Be(640);
+        }
+
+        [Fact]
+        public void Part2_GetResult()
+        {
+            Spinlock
+                .Part2(349)
+                .Should()
+                .Be(47949463);
+        }
+
+        [Fact]
+        public void Part1_Test()
+        {
+            Spinlock
+                .StartInsertion(3)
+                .Should()
+                .Be(638);
+        }
+    }
+}
