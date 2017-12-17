e6ea14d449c735eefd221b79136b1f1ecfd646f6
nils-larsen <nilsfredriklarsen@gmail.com>
2017-12-17 14:54:39 +0100

    Day 15 finished and refactored

---
+++ AdventOfCode.Day15/Day15Tests.cs
@@ -0,0 +1,53 @@
+using Xunit;
+using FluentAssertions;
+using System.Linq;
+
+namespace AdventOfCode.Day15
+{
+    public class TestDuelingGenerator
+    {
+        [Fact]
+        public void Part1_GetResult()
+        {
+            Dueling
+                .Generator(516, 16807)
+                .CompareLowerBits(Dueling.Generator(190, 48271), 40000000)
+                .Sum()
+                .Should()
+                .Be(597);
+        }
+
+        [Fact]
+        public void Part2_GetResult()
+        {
+            Dueling
+                .Generator(516, 16807, 4)
+                .CompareLowerBits(Dueling.Generator(190, 48271, 8), 5000000)
+                .Sum()
+                .Should()
+                .Be(303);
+        }
+
+        [Fact]
+        public void Part1_Test()
+        {
+            Dueling
+                .Generator(65, 16807)
+                .CompareLowerBits(Dueling.Generator(8921, 48271), 5)
+                .Sum()
+                .Should()
+                .Be(1);
+        }
+
+        [Fact]
+        public void Part2_Test()
+        {
+            Dueling
+                .Generator(65, 16807, 4)
+                .CompareLowerBits(Dueling.Generator(8921, 48271, 8), 5)
+                .Sum()
+                .Should()
+                .Be(0);
+        }
+    }
+}
---
+++ AdventOfCode.Day15/DuelingGenerators.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode.Day15
+{
+    public static class Dueling
+    {
+        public static IEnumerator<long> Generator(this long num, int factor, int modulos = 1)
+        {
+            while (true)
+            {
+                var next = (num * factor) % 2147483647;
+
+                if (next % modulos == 0)
+                {
+                    yield return next;
+                }
+                num = next;
+            }
+        }
+
+        public static IEnumerable<int> CompareLowerBits(this IEnumerator<long> listA, IEnumerator<long> listB, int repetitions)
+        {
+            for (var i = 0; i < repetitions; i++)
+            {
+                listA.MoveNext();
+                listB.MoveNext();
+
+                if ((listA.Current & 0xFFFF) == (listB.Current & 0xFFFF))
+                    yield return 1;
+            }
+        }
+    }
+}
