e7c406da6f2f5163968307a85eb02e16ca7db59b
nils-larsen <nilsfredriklarsen@gmail.com>
2018-11-11 13:43:47 +0100

    refactoring

---
+++ src/AdventOfCode.2015/AdventOfCode.Day3/Day3Tests.cs
@@ -0,0 +1,68 @@
+using System.Linq;
+using System.IO;
+using Xunit;
+using FluentAssertions;
+
+namespace AdventOfCode.Day3
+{
+    public class Day3Tests
+    {
+        [Fact]
+        public void GetResult_Part1()
+        {
+            File.ReadAllText("Day3Input.txt")
+                .ToCharArray()
+                .DeliverBy(nrOfSantas: 1)
+                .Distinct()
+                .ToList()
+                .Count()
+                .Should()
+                .Be(2572);
+        }
+
+        [Fact]
+        public void GetResult_Part2()
+        {
+            File.ReadAllText("Day3Input.txt")
+                .ToCharArray()
+                .DeliverBy(nrOfSantas: 2)
+                .Distinct()
+                .ToList()
+                .Count()
+                .Should()
+                .Be(2631);
+        }
+
+        [Theory]
+        [InlineData(">", 2)]
+        [InlineData("^>v<", 4)]
+        [InlineData("^v^v^v^v^v", 2)]
+        public void Test1(string input, int expected)
+        {
+            input
+                .ToCharArray()
+                .DeliverBy(nrOfSantas: 1)
+                .Distinct()
+                .ToList()
+                .Count()
+                .Should()
+                .Be(expected);
+        }
+
+        [Theory]
+        [InlineData("^v", 3)]
+        [InlineData("^>v<", 3)]
+        [InlineData("^v^v^v^v^v", 11)]
+        public void Test2(string input, int expected)
+        {
+            input
+                .ToCharArray()
+                .DeliverBy(nrOfSantas: 2)
+                .Distinct()
+                .ToList()
+                .Count()
+                .Should()
+                .Be(expected);
+        }
+    }
+}
---
+++ src/AdventOfCode.2015/AdventOfCode.Day3/Santa.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+namespace AdventOfCode.Day3
+{
+    public static class Santa
+    {
+        public static List<(int, int)> DeliverBy(this char[] direction, int nrOfSantas)
+        {
+            var visitedCoordinate = new List<(int, int)>();
+            var coordinate = (X: 0, Y: 0);
+
+            for (var i = 0; i < nrOfSantas; i++)
+            {
+                visitedCoordinate.Add(coordinate);
+            }
+
+            for (var i = 0; i < direction.Length; i++)
+            {
+                var dir = direction[i];
+
+                if (i % 2 == 0)
+                {
+                    coordinate = visitedCoordinate[i];
+                    coordinate = Move(dir, coordinate);
+                }
+                else
+                {
+                    coordinate = visitedCoordinate[i];
+                    coordinate = Move(dir, coordinate);
+                }
+                visitedCoordinate.Add(coordinate);
+            }
+            return visitedCoordinate;
+        }
+
+        public static (int, int) Move(char direction, (int X, int Y) coordinate)
+        {
+            if (direction.Equals('>'))
+                coordinate.X += 1;
+
+            else if (direction.Equals('v'))
+                coordinate.Y -= 1;
+
+            else if (direction.Equals('<'))
+                coordinate.X -= 1;
+
+            else if (direction.Equals('^'))
+                coordinate.Y += 1;
+
+            return coordinate;
+        }
+    }
+}
