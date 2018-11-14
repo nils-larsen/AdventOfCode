4d6b27d0defadac216dc3aa300c1d141ca314d43
nils-larsen <nilsfredriklarsen@gmail.com>
2018-11-14 14:45:19 +0100

    Day 5 done

--- src/AdventOfCode.2015/AdventOfCode.Day5/Day5Tests.cs
+++ src/AdventOfCode.2015/AdventOfCode.Day5/Day5Tests.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using System.IO;
 using FluentAssertions;
 using Xunit;
+using System.Collections.Generic;
 
 namespace AdventOfCode.Day5
 {
@@ -11,13 +12,21 @@ namespace AdventOfCode.Day5
         public void GetResult_Part1()
         {
             File.ReadLines("Day5Input.txt")
-                .ToList()
-                .Select(x => x.HasThreeVowels()
-                              .HasDoubleLetters()
-                              .HasNoForbiddenStrings())
-                .Count(x => x.Equals(1))
+                .Count(x => x.HasThreeVowels()
+                         && x.HasDoubleLetters()
+                         && x.HasNoForbiddenStrings())
                 .Should()
                 .Be(236);
+        }
+
+        [Fact]
+        public void GetResult_Part2()
+        {
+            File.ReadLines("Day5Input.txt")
+                .Count(x => x.HasDoublePairs()
+                         && x.HasRepeatingLetter())
+                .Should()
+                .Be(51);
         }
 
         [Theory]
@@ -28,10 +37,28 @@ namespace AdventOfCode.Day5
         [InlineData("dvszwmarrgswjxmb", 0)]
         public void Test1(string input, int expected)
         {
-            input
-                .HasThreeVowels()
-                .HasDoubleLetters()
-                .HasNoForbiddenStrings()
+            var inputArray = new List<string> { input };
+
+            inputArray
+                .Count(x => x.HasThreeVowels()
+                         && x.HasDoubleLetters()
+                         && x.HasNoForbiddenStrings())
+                .Should()
+                .Be(expected);
+        }
+
+        [Theory]
+        [InlineData("qjhvhtzxzqqjkmpb", 1)]
+        [InlineData("xxyxx", 1)]
+        [InlineData("uurcxstgmygtbstg", 0)]
+        [InlineData("ieodomkazucvgmuy", 0)]
+        public void Test2(string input, int expected)
+        {
+            var inputArray = new List<string> { input };
+
+            inputArray
+                .Count(x => x.HasDoublePairs()
+                         && x.HasRepeatingLetter())
                 .Should()
                 .Be(expected);
         }
--- src/AdventOfCode.2015/AdventOfCode.Day5/SantasRules.cs
+++ src/AdventOfCode.2015/AdventOfCode.Day5/SantasRules.cs
@@ -1,46 +1,67 @@
 ﻿using System.Linq;
+
 namespace AdventOfCode.Day5
 {
     public static class Rules
     {
-        public static string HasThreeVowels(this string input)
+        public static bool HasThreeVowels(this string input)
         {
             var niceVowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
 
             int vowelCount = input.Count(c => niceVowels.Contains(c));
 
-            return vowelCount > 2 ? input : "";
+            return vowelCount > 2;
         }
 
-        public static string HasDoubleLetters(this string input)
+        public static bool HasDoubleLetters(this string input)
         {
-            if (input == "")
-                return "";
-
             var all = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+
             foreach (var c in all.Where(c => input.Contains($"{c}{c}")))
             {
-                return input;
+                return true;
             }
-            return "";
+            return false;
         }
 
-        public static int HasNoForbiddenStrings(this string input)
+        public static bool HasNoForbiddenStrings(this string input)
         {
-            if (input == "")
-                return 0;
-
             var forbidden = new string[] { "ab", "cd", "pq", "xy" };
 
             foreach (var f in forbidden)
             {
                 if (input.Contains(f))
                 {
-                    return 0;
+                    return false;
                 }
             }
+            return true;
+        }
 
-            return 1;
+        public static bool HasDoublePairs(this string input)
+        {
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                var pair = input.Substring(i, 2);
+
+                if (input.IndexOf(pair, i + 2) != -1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool HasRepeatingLetter(this string input)
+        {
+            var letters = input.ToCharArray();
+
+            for (var i = 0; i < letters.Length - 2; i++)
+            {
+                if (letters[i] == letters[i + 2])
+                    return true;
+            }
+            return false;
         }
     }
 }
